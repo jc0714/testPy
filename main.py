fd5d7380f51fd733f9771112394d7e96700de8d3
Joyce <0714joyce@gmail.com>
2024-12-20 11:15:58 +0800

    add sum teast

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ def greet(name):
 
 if __name__ == "__main__":
     print(greet("macOS"))
+
+def sum(int, int):
+    return f"{int + int}"
---
+++ test_sum.py
@@ -0,0 +1,6 @@
+#test_sum.py 
+
+from main import sum
+
+def test_sum():
+    assert sum(22, 12) == "34"
